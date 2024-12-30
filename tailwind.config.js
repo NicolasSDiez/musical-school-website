a03a1ceb4f7c87c0011bb1ee3e85d52f0698099c
NicolasSDiez <nicolas.diez1s@gmail.com>
2024-12-30 00:18:44 -0300

    Configuración inicial del proyecto con Tailwind CSS y React

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Esta línea incluye todos los archivos de tu carpeta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
