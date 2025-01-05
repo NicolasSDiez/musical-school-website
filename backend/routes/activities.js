56be8172d29474526b076d795f0758beed7464bb
NicolasSDiez <nicolas.diez1s@gmail.com>
2025-01-05 18:39:12 -0300

    prueba server

--- backend/routes/activities.js
+++ backend/routes/activities.js
@@ -2,7 +2,7 @@ import express from "express";
 import activitiesController from "/controllers/activitiesController.js";
 const router = express.Router();
 
-router.get("/", activitiesController.getAllActivities);
+router.get("/a", activitiesController.getAllActivities);
 router.post("/", activitiesController.createActivity);
 router.get("/:id", activitiesController.getActivityById);
 
